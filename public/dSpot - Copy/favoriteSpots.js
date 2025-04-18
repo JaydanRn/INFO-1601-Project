34dc1c203e52f3b61ebaccce2e3e826fb269eb24
JaydanRn <jaydanramkissoon@gmail.com>
2025-04-17 21:21:50 -0400

    Added copy

---
+++ public/dSpot - Copy/favoriteSpots.js
@@ -0,0 +1,17 @@
+const favoriteSpots = [
+    {
+      id: 101,
+      title: "Sunset Doubles",
+      image: "assets/images/doubles.jpg",
+      rating: 4.8,
+      location: "Maracas Bay"
+    },
+    {
+      id: 102,
+      title: "City Bake & Shark",
+      image: "assets/images/bake.jpg",
+      rating: 4.6,
+      location: "Port of Spain"
+    }
+  ];
+  
---
+++ public/dSpot - Copy/spots.js
@@ -0,0 +1,29 @@
+// spots.js
+const featuredSpots = [
+    {
+      id: 1,
+      title: "Doubles by Junction",
+      image: "assets/images/doubles.jpg"
+    },
+    {
+      id: 2,
+      title: "Pie Man by Curepe",
+      image: "assets/images/pie.jpg"
+    },
+    {
+      id: 3,
+      title: "Bake & Shark",
+      image: "assets/images/bake.jpg"
+    },
+    {
+      id: 4,
+      title: "Chutney Chicken Van",
+      image: "assets/images/chicken.jpg"
+    },
+    {
+      id: 5,
+      title: "Roti Stop",
+      image: "assets/images/roti.jpg"
+    }
+  ];
+  
